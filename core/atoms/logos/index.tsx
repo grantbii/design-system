3753192fd8b9d2f497a417aac7c00fbed0f34890
Jason Ngo <jason@grantbii.com>
2025-07-16 18:45:35 +0700

    Fix brand logo paths

--- core/atoms/logos/index.tsx
+++ core/atoms/logos/index.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import darkLogo from "./images/brand_logo-dark.webp";
+import lightLogo from "./images/brand_logo-light.webp";
 
 type BrandLogoProps = {
   isDarkTheme?: boolean;
@@ -10,7 +12,7 @@ export const BrandLogo = ({
   alt = "Grantbii",
 }: BrandLogoProps) => (
   <Image
-    src={`/logos/brand_logo-${isDarkTheme ? "dark" : "light"}.webp`}
+    src={isDarkTheme ? darkLogo : lightLogo}
     alt={alt}
     width={250}
     height={80}
