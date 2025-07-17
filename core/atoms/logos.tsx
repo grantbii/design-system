98df4f57f10e0f8f0876e063a75d4b528aba07b5
Jason Ngo <jason@grantbii.com>
2025-07-17 10:35:17 +0700

    Parametrize brand logo dimensions

--- core/atoms/logos.tsx
+++ core/atoms/logos.tsx
@@ -3,18 +3,22 @@ import darkLogo from "../../public/logos/brand_logo-dark.webp";
 import lightLogo from "../../public/logos/brand_logo-light.webp";
 
 type BrandLogoProps = {
+  width?: number;
+  height?: number;
   isDarkTheme?: boolean;
   alt?: string;
 };
 
 export const BrandLogo = ({
+  width = 250,
+  height = 80,
   isDarkTheme = true,
   alt = "Grantbii",
 }: BrandLogoProps) => (
   <Image
     src={isDarkTheme ? darkLogo : lightLogo}
     alt={alt}
-    width={250}
-    height={80}
+    width={width}
+    height={height}
   />
 );
