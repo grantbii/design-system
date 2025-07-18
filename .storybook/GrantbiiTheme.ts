3d82dc1e551065836e963bcf4945db63b5d40122
Jason Ngo <jason@grantbii.com>
2025-07-18 08:26:46 +0700

    Create custom theme for storybook

---
+++ .storybook/GrantbiiTheme.ts
@@ -0,0 +1,11 @@
+import { create } from "storybook/theming";
+
+const grantbiiTheme = create({
+  base: "light",
+  brandTitle: "Grantbii",
+  brandImage: "../public/logos/brand_logo-dark.webp",
+  brandUrl: "https://grantbii.com",
+  brandTarget: "_blank",
+});
+
+export default grantbiiTheme;
---
+++ .storybook/manager.ts
@@ -0,0 +1,6 @@
+import { addons } from "storybook/manager-api";
+import grantbiiTheme from "./GrantbiiTheme";
+
+addons.setConfig({
+  theme: grantbiiTheme,
+});
