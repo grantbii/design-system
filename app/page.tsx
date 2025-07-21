899d7832b1805b68e591e00f565d5694fce2d40f
Jason Ngo <jason@grantbii.com>
2025-07-21 13:56:06 +0700

    Refactor published folder structure

--- app/page.tsx
+++
@@ -1,3 +0,0 @@
-const HomePage = () => <main>Hello Grantbii</main>;
-
-export default HomePage;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,5 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import { FlatCompat } from "@eslint/eslintrc";
+import storybook from "eslint-plugin-storybook";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
@@ -16,6 +14,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
+      "@next/next/no-html-link-for-pages": "off",
       "@typescript-eslint/no-unused-vars": [
         "error",
         { argsIgnorePattern: "^_" },
