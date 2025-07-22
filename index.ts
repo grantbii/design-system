a05cf9fcb60230a243b96ac5facf01a03dde7686
Jason Ngo <jason@grantbii.com>
2025-07-22 10:47:17 +0700

    Add country flags to design system

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export * from "@phosphor-icons/react";
+export * as Flags from "country-flag-icons/react/3x2";
 export { default as BrandLogo } from "./core/atoms/BrandLogo";
 export * from "./core/atoms/colors";
 export { default as GlobalStyle } from "./core/global/GlobalStyle";
