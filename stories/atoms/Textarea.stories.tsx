a0f3c6dc440788398db6e2957151acab6169ea24
Jason Ngo <jason@grantbii.com>
2025-08-13 01:04:16 +0800

    Add textarea to design system

---
+++ stories/atoms/Textarea.stories.tsx
@@ -0,0 +1,23 @@
+import { Textarea } from "@/.";
+import { Meta, StoryObj } from "@storybook/nextjs-vite";
+
+const meta: Meta<typeof Textarea> = {
+  title: "Atoms/Textarea",
+  component: Textarea,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const BasicExample: Story = {
+  args: { placeholder: "Type here" },
+};
+
+export const WithValidation: Story = {
+  args: { placeholder: "Must be at least 10 characters long", minLength: 10 },
+};
