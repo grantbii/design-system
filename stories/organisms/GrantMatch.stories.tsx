3c611ef7eca444f000f45b4cc09b47390aa84ca0
Jason Ngo <jason@grantbii.com>
2025-08-16 11:08:30 +0800

    Perform grant match on change in query

--- stories/organisms/GrantMatch.stories.tsx
+++ stories/organisms/GrantMatch.stories.tsx
@@ -2,35 +2,50 @@ import { GrantMatch } from "@/.";
 import { useGrantMatchQueryItems } from "@/core/organisms/GrantMatch";
 import { GrantMatchQuery } from "@grantbii/ui-base/match/models";
 import { Meta, StoryObj } from "@storybook/nextjs-vite";
+import { useState } from "react";
 import styled from "styled-components";
 
 type GrantMatchExampleProps = {
   isModalFullScreen?: boolean;
 };
 const GrantMatchExample = ({ isModalFullScreen }: GrantMatchExampleProps) => {
-  const { query, updateQuery, removeQueryFile, removeQueryText, resetQuery } =
-    useGrantMatchQueryItems();
+  const [status, setStatus] = useState("Pending input");
 
-  const onPerformGrantMatch = (newQuery: GrantMatchQuery) => {
-    updateQuery(newQuery);
-    console.log("finding grants...");
+  const performGrantMatch = (newQuery: GrantMatchQuery) => {
+    const fileNames = newQuery.files.map((file) => file.name).join(", ");
+    setStatus(
+      `Finding grants using files [${fileNames}] and text [${newQuery.text}]`,
+    );
+    setTimeout(() => setStatus("Found grants"), 3000);
   };
+
+  const resetGrantMatch = () => setStatus("Pending input");
+
+  const grantMatchQueryProps = useGrantMatchQueryItems(
+    performGrantMatch,
+    resetGrantMatch,
+  );
 
   return (
     <Container>
+      <GrantMatchStatus>Status: {status}</GrantMatchStatus>
+
       <GrantMatch
-        query={query}
-        onPerformGrantMatch={onPerformGrantMatch}
-        removeQueryFile={removeQueryFile}
-        removeQueryText={removeQueryText}
-        resetQuery={resetQuery}
+        {...grantMatchQueryProps}
         isModalFullScreen={isModalFullScreen}
       />
     </Container>
   );
 };
 
+const GrantMatchStatus = styled.p`
+  margin: 0px 16px;
+`;
+
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
   width: 90vw;
 `;
 
