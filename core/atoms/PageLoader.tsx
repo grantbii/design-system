262a91d9c4f274c41766798859c8fd3cab3c038a
Jason Ngo <jason@grantbii.com>
2025-08-20 18:06:44 +0800

    Fix page loader not fullscreen

--- core/atoms/PageLoader.tsx
+++ core/atoms/PageLoader.tsx
@@ -3,21 +3,33 @@ import { LoaderSizeProps } from "react-spinners/helpers/props";
 import styled from "styled-components";
 import { Colors } from "../foundations";
 
+type PageLoaderProps = {
+  pageWidth?: string;
+  pageHeight?: string;
+} & LoaderSizeProps;
+
 /**
  * The animation to show when loading the whole page
  */
 const PageLoader = ({
+  pageWidth,
+  pageHeight,
   color = Colors.accent.blue1,
   size = 32,
   ...restOfProps
-}: LoaderSizeProps) => (
-  <BasePageLoader>
+}: PageLoaderProps) => (
+  <Background $pageWidth={pageWidth} $pageHeight={pageHeight}>
     <MoonLoader color={color} size={size} {...restOfProps} />
-  </BasePageLoader>
+  </Background>
 );
 
 export default PageLoader;
 
-const BasePageLoader = styled.div`
-  margin: auto;
+const Background = styled.div<{ $pageWidth?: string; $pageHeight?: string }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: ${({ $pageWidth = "100%" }) => $pageWidth};
+  height: ${({ $pageHeight = "100%" }) => $pageHeight};
 `;
