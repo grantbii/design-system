fb994d5250515ee9ea975d2f64c3c025300310e3
Jason Ngo <jason@grantbii.com>
2025-08-22 13:01:18 +0800

    Fix bottom of modal getting cut off on mobile

--- stories/atoms/PageLoader.stories.tsx
+++ stories/atoms/PageLoader.stories.tsx
@@ -2,9 +2,13 @@ import { PageLoader } from "@/.";
 import { Meta, StoryObj } from "@storybook/nextjs-vite";
 import styled from "styled-components";
 
-const PageLoaderExample = () => (
+type PageLoaderExampleProps = {
+  isPacman?: boolean;
+};
+
+const PageLoaderExample = ({ isPacman }: PageLoaderExampleProps) => (
   <BaseExample>
-    <PageLoader />
+    <PageLoader isPacman={isPacman} />
   </BaseExample>
 );
 
@@ -26,6 +30,10 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const GrantbiiApp: Story = {
   args: {},
+};
+
+export const AdminConsole: Story = {
+  args: { isPacman: true },
 };
