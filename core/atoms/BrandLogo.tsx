3c52df7a27ae516c4b3642f5170e3247da1191a7
Jason Ngo <jason@grantbii.com>
2025-08-26 22:58:10 +0800

    Fix brand logo

--- core/atoms/BrandLogo.tsx
+++ core/atoms/BrandLogo.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import Image from "next/image";
+import styled from "styled-components";
 import darkLogo from "../assets/logos/brand_logo-dark.webp";
 import lightLogo from "../assets/logos/brand_logo-light.webp";
-import styled from "styled-components";
 import { Responsive } from "../foundations";
 
 type BrandLogoProps = {
@@ -15,7 +15,13 @@ const BrandLogo = ({
   isDarkTheme = true,
   alt = "Grantbii",
 }: BrandLogoProps) => (
-  <CustomImage src={isDarkTheme ? darkLogo : lightLogo} alt={alt} priority />
+  <CustomImage
+    src={isDarkTheme ? darkLogo : lightLogo}
+    width={250}
+    height={80}
+    alt={alt}
+    priority
+  />
 );
 
 export default BrandLogo;
