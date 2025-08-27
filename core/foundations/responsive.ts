fe29e2ed788bc4fbbd61f3567a4b96429d5226eb
Jason Ngo <jason@grantbii.com>
2025-08-28 01:29:46 +0800

    Refactor grant match

--- core/foundations/responsive.ts
+++ core/foundations/responsive.ts
@@ -1,3 +1,25 @@
+import styled from "styled-components";
+
 export const WIDTH_BREAKPOINTS = {
   laptop: "1024px",
 };
+
+export const SmallScreenOnly = styled.div`
+  @media (width < ${WIDTH_BREAKPOINTS.laptop}) {
+    display: inline;
+  }
+
+  @media (width >= ${WIDTH_BREAKPOINTS.laptop}) {
+    display: none;
+  }
+`;
+
+export const BigScreenOnly = styled.div`
+  @media (width < ${WIDTH_BREAKPOINTS.laptop}) {
+    display: none;
+  }
+
+  @media (width >= ${WIDTH_BREAKPOINTS.laptop}) {
+    display: inline;
+  }
+`;
