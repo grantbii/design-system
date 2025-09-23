1471cfca210ad82b2436b0800d0e71a6ff5dddd7
Jason Ngo <jason@grantbii.com>
2025-09-23 13:01:25 +0800

    Update colours

--- core/foundations/colors.ts
+++ core/foundations/colors.ts
@@ -1,46 +1,46 @@
 export const main = {
-  grantbiiBlue: "#16538a",
-  grantbiiOrange: "#f3924d",
+  grantbiiBlue: "#16538A",
+  grantbiiOrange: "#EBA338",
 };
 
 export const base = {
   black: "#232022",
-  white: "#ffffff",
+  white: "#FFFFFF",
 };
 
 export const neutral = {
   grey1: "#999999",
-  grey2: "#cccccc",
-  grey3: "#eeeeee",
-  grey4: "#fafafa",
+  grey2: "#CCCCCC",
+  grey3: "#EEEEEE",
+  grey4: "#FAFAFA",
 };
 
 export const accent = {
   // usually includes dangerous, wrong or rejected information
-  red1: "#c02318",
-  red2: "#f88e86",
-  red3: "#fcc7c3",
+  red1: "#C02318",
+  red2: "#F88E86",
+  red3: "#FFE9E7",
   // usually indicates warning, progressing, etc
-  yellow1: "#edab03",
-  yellow2: "#f4cc68",
-  yellow3: "#fae6b3",
+  yellow1: "#EDAB03",
+  yellow2: "#F4CC68",
+  yellow3: "#FFF9E8",
   // usually indicates success, correct, passed, etc
-  green1: "#08a45e",
-  green2: "#6bc89e",
-  green3: "#b5e4cf",
+  green1: "#08A45E",
+  green2: "#6BC89E",
+  green3: "#EBFFF6",
   // usually indicates info, no emotion
-  blue1: "#1878e4",
-  blue2: "#74aeef",
-  blue3: "#bad6f7",
+  blue1: "#1878E4",
+  blue2: "#74AEEF",
+  blue3: "#ECF5FF",
 };
 
 export const typography = {
-  blackHigh: "#000000e5",
+  blackHigh: "#000000E5",
   blackMedium: "#00000099",
-  blackLow: "#0000004d",
-  whiteHigh: "#ffffff",
-  whiteMedium: "#ffffff99",
-  whiteLow: "#0000001a",
+  blackLow: "#0000004D",
+  whiteHigh: "#FFFFFF",
+  whiteMedium: "#FFFFFF99",
+  whiteLow: "#0000001A",
 };
 
 export const semantic = {
