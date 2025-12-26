e0629c81d82b7fb595aa91544a9c2da175f849e4
Jason Ngo <jason@grantbii.com>
2025-12-26 13:58:59 +0800

    Add location icon

--- core/integrations/mappings.ts
+++ core/integrations/mappings.ts
@@ -1,6 +1,5 @@
 import type { BaseEnumType } from "@grantbii/ui-core/enums";
-import { Location } from "@grantbii/ui-core/location/enums";
-import { Flags, type Option } from "../foundations";
+import type { Option } from "../foundations";
 
 export const mapEnumToOptions = <EnumType extends BaseEnumType>(
   enumType: EnumType,
@@ -8,18 +7,3 @@ export const mapEnumToOptions = <EnumType extends BaseEnumType>(
   Object.values(enumType)
     .filter((value) => value !== enumType.UNKNOWN)
     .map((value) => ({ label: value, value }));
-
-export const LOCATION_FLAG_MAP: {
-  [location in Location]: Flags.FlagComponent;
-} = {
-  [Location.SINGAPORE]: Flags.SG,
-  [Location.HONG_KONG]: Flags.HK,
-  [Location.MALAYSIA]: Flags.MY,
-  [Location.SOUTH_KOREA]: Flags.KR,
-  [Location.JAPAN]: Flags.JP,
-  [Location.CHINA]: Flags.CN,
-  [Location.UNITED_KINGDOM]: Flags.GB,
-  [Location.UNITED_STATES]: Flags.US,
-  [Location.OTHERS]: Flags.SG,
-  [Location.UNKNOWN]: Flags.SG,
-};
