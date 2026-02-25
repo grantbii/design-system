addf3511a69c0134a009bbd262a50c0d9061d196
Jason Ngo <jason@grantbii.com>
2026-02-26 00:20:31 +0800

    Remove icons from input

--- core/atoms/Input.tsx
+++ core/atoms/Input.tsx
@@ -1,52 +1,17 @@
 import type { DetailedHTMLProps, InputHTMLAttributes } from "react";
 import styled from "styled-components";
-import { Colors, Icons, type Option } from "../foundations";
+import { Colors, type Option } from "../foundations";
 import { InputValidation, type InputValidationProps } from "./shared";
 
 type InputProps = {
-  LeftIcon?: Icons.Icon;
-  RightIcon?: Icons.Icon;
-  iconColor?: string;
   datalistId?: string;
   datalistOptions?: Option[];
 } & InputValidationProps &
   DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
 
-const Input = ({
-  LeftIcon,
-  RightIcon,
-  iconColor,
-  datalistOptions,
-  datalistId,
-  ...inputProps
-}: InputProps) => (
+const Input = ({ datalistOptions, datalistId, ...inputProps }: InputProps) => (
   <>
-    {LeftIcon ? (
-      <LeftIcon
-        color={iconColor}
-        size={20}
-        style={{ position: "relative", left: 28, top: 4 }}
-      />
-    ) : (
-      <></>
-    )}
-
-    <BaseInput
-      {...inputProps}
-      list={datalistId}
-      $hasLeftIcon={!!LeftIcon}
-      $hasRightIcon={!!RightIcon}
-    />
-
-    {RightIcon ? (
-      <RightIcon
-        color={iconColor}
-        size={20}
-        style={{ position: "relative", right: 28, top: 4 }}
-      />
-    ) : (
-      <></>
-    )}
+    <BaseInput {...inputProps} list={datalistId} />
 
     {datalistId && datalistOptions ? (
       <Datalist id={datalistId} options={datalistOptions} />
@@ -58,14 +23,8 @@ const Input = ({
 
 export default Input;
 
-type BaseInputProps = {
-  $hasLeftIcon: boolean;
-  $hasRightIcon: boolean;
-} & InputValidationProps;
-
-const BaseInput = styled.input<BaseInputProps>`
-  padding: ${({ $hasLeftIcon, $hasRightIcon }) =>
-    `12px ${$hasRightIcon ? "32px" : "16px"} 12px ${$hasLeftIcon ? "32px" : "16px"}`};
+const BaseInput = styled.input<InputValidationProps>`
+  padding: 12px 16px;
   background-color: ${Colors.base.white};
   border-radius: 8px;
 
