a36fb9b9bdfcca21f4ba4c9c8ce36aa29d353faf
Jason Ngo <jason@grantbii.com>
2026-03-17 10:53:20 +0800

    Export all loaders

--- core/foundations/index.ts
+++ core/foundations/index.ts
@@ -1,6 +1,7 @@
-export * as Responsive from "./responsive";
 export * as Colors from "./colors";
 export * as Flags from "./flags";
 export * as Icons from "./icons";
+export * as Loaders from "./loaders";
+export * as Responsive from "./responsive";
 export type * from "./types";
 export * as Typography from "./typography";
