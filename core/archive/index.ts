eb21f84a3beac269a25653b4950892e75f4f41b3
francistriesscience <hello@francistries.science>
2026-07-09 09:39:38 +0800

    Remove Tally Modal

--- core/archive/index.ts
+++ core/archive/index.ts
@@ -1,4 +1,3 @@
 export { default as LocationIcon } from "./LocationIcon";
 export { default as RadioButtons, type RadioOption } from "./RadioButtons";
-export { default as TallyModal } from "./TallyModal";
 export { default as YesNoOptions } from "./YesNoOptions";
